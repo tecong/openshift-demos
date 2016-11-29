9320fe58200406a619dd4c8e7282180393e88f4d
Sillanpaa Janne <janne.sillanpaa@tieto.com>
2016-11-29 09:20:34 +0200

    Robot framework demo container

---
+++ robotframework-demo/run.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+# Entry script to start Xvfb and set display
+set -e
+
+# Set the defaults
+DEFAULT_LOG_LEVEL="INFO" # Available levels: TRACE, DEBUG, INFO (default), WARN, NONE (no logging)
+DEFAULT_RES="1280x1024x24"
+DEFAULT_DISPLAY=":99"
+DEFAULT_ROBOT_TESTS="false"
+DEFAULT_ROBOT_OUTPUT_DIRECTORY="/robot/results"
+
+# Use default if none specified as env var
+LOG_LEVEL=${LOG_LEVEL:-$DEFAULT_LOG_LEVEL}
+RES=${RES:-$DEFAULT_RES}
+DISPLAY=${DISPLAY:-$DEFAULT_DISPLAY}
+ROBOT_TESTS=${ROBOT_TESTS:-$ROBOT_TESTS}
+ROBOT_OUTPUT_DIRECTORY=${ROBOT_OUTPUT_DIRECTORY:-$DEFAULT_ROBOT_OUTPUT_DIRECTORY}
+
+if [[ "${ROBOT_TESTS}" == "false" ]]; then
+  echo "Error: Please specify the robot test or directory as env var ROBOT_TESTS"
+  exit 1
+fi
+
+# Start Xvfb
+echo -e "Starting Xvfb on display ${DISPLAY} with res ${RES}"
+Xvfb ${DISPLAY} -ac -screen 0 ${RES} +extension RANDR &
+export DISPLAY=${DISPLAY}
+
+# Creating output directory
+if [ "${ROBOT_OUTPUT_DIRECTORY}" ]; then
+   mkdir -p ${ROBOT_OUTPUT_DIRECTORY}
+fi 
+
+# Execute tests
+echo -e "Executing robot tests at log level ${LOG_LEVEL}"
+
+pybot --loglevel ${LOG_LEVEL} -d ${ROBOT_OUTPUT_DIRECTORY} ${ROBOT_TESTS}
+
+# Stop Xvfb
+kill -9 $(pgrep Xvfb)
